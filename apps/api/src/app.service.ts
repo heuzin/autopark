a75fca3464e9bc2f1a2abd63a9e86116fdac5a58
Matheus Silva <msilvaqs@outlook.com>
2024-11-09 13:11:25 -0500

    Craeting shared library

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { add } from '@autopark/sample-lib';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + add(1, 5);
   }
 }
